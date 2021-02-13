20f901f9288d25eafc06f29d45eef383c78e839f
T Schway <tony.schwebach@gmail.com>
2021-02-13 11:56:07 -0500

    added employee grid

--- src/containers/EmployeeGrid/EmployeeGrid.jsx
+++ src/containers/EmployeeGrid/EmployeeGrid.jsx
@@ -1,8 +1,4 @@
 // onClick for table headings to setState of sortBy
-// render table and table headings
-// filter on state.searchTerm (do not filter imageURL)
-// sort asc or desc based on state.sortBy
-// map over state of employees to render Employee components (reference activity 16 - friends)
 
 import React, { Component } from "react";
 import API from "../../utils/API";
@@ -12,14 +8,49 @@ class EmployeeGrid extends Component {
   state = { employees: "", sortBy: "", searchTerm: "" };
   // componentDidMount renders grid
   componentDidMount() {
-    const numberOfEmployees = 20 //else .getEmployees defaults to 50
-    API.getEmployees(numberOfEmployees).then((res) => console.log(res.data.results));
+    const numberOfEmployees = 20; //else .getEmployees defaults to 50
+    API.getEmployees(numberOfEmployees)
+      .then((res) => {
+        this.setState({ employees: res.data.results });
+        console.log(this.state.employees);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
   // make api call with axios to setState with response (reference activity 20 - omdb movies)
 
   render() {
-    return <div>EmployeeGrid</div>;
+    return (
+      <div className="container mt-6">
+        {/* render table and table headings */}
+        <table className="table is-striped is-narrow is-hoverable is-fullwidth">
+          <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Phone</th>
+              <th>Email</th>
+              <th>DOB</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td><img alt="employee picture of name" src=""/></td>
+              <td>Johnny Rose</td>
+              <td>555-123-4567</td>
+              <td>johnny@rosebud.com</td>
+              <td>1/1/1900</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      /* filter on state.searchTerm (do not filter imageURL) */
+      /* sort asc or desc based on state.sortBy */
+      /* map over state of employees to render Employee components (reference activity 16 - friends) */
+    );
   }
 }
 
