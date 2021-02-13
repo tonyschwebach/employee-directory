75f463c403cf4a2d979e3d36df8c0931a477999c
T Schway <tony.schwebach@gmail.com>
2021-02-13 10:33:12 -0500

    added hero

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./Banner.css";
+
+const Banner = () => {
+  return (
+    <div class="hero">
+    <div class="hero-body">
+      <h1 class="title">
+        Employee Directory
+      </h1>
+      <h3 class="subtitle">
+        Click on the the column headings to sort by heading or use the search box to filter your results.
+      </h3>
+    </div>
+  </div>
+  );
+};
+
+export default Banner;
